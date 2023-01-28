c53ad27120423513b9068b9cbccd794ce589fc00
Charlie Short <charliemshort@gmail.com>
2023-01-28 18:12:22 +1100

    Update gatsby-node.js
    fetch cashrewards

--- gatsby-node.js
+++ gatsby-node.js
@@ -83,3 +83,28 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+// CHARLIES STUFF
+const fetch = (...args) =>
+  import(`node-fetch`).then(({ default: fetch }) => fetch(...args))
+
+exports.sourceNodes = async ({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  // get data from GitHub API at build time
+  const result = await fetch(`https://www.cashrewards.com.au/api/offers/v1/Offers?clientId=1000000&isFeatured=false`)
+  const resultData = await result.json()
+  // create node for build time data example in the docs
+  createNode({
+    // nameWithOwner and url are arbitrary fields from the data
+    title: resultData.data.title,
+    // required fields
+    id: resultData.data.id,
+    parent: null,
+    children: [],
+    internal: {
+      type: `Example`,
+      contentDigest: createContentDigest(resultData),
+    },
+  })
+}
