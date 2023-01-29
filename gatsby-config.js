f83b7a6fd2cd6318d84d6ed8e8290b6efb9c7d45
Charlie Short <charliemshort@gmail.com>
2023-01-29 22:07:47 +1100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-react-helmet",
-    "plugins/source-plugin",
+    "source-plugin",
     {
       resolve: "gatsby-plugin-sass",
       options: {
