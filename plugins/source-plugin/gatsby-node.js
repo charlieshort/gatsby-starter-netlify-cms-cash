ef3fc41a1a00d53a1d920805e99fc8129f3da4f1
Charlie Short <charliemshort@gmail.com>
2023-01-29 22:24:40 +1100

    aniox

--- plugins/source-plugin/gatsby-node.js
+++ plugins/source-plugin/gatsby-node.js
@@ -1,6 +1,6 @@
 /**
  * Implement Gatsby's Node APIs in this file.
- *d
+ *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 // You can delete this file if you're not using it
@@ -11,4 +11,54 @@
  *
  * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
  */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+const axios = require('axios');
+
+exports.onPreInit = () => console.log("Loaded ships");
+
+// constants for your GraphQL Post and Author types
+const SHIP_NODE_TYPE = `Ship`;
+
+exports.sourceNodes = async ({
+  actions,
+  createContentDigest,
+  createNodeId,
+  getNodesByType,
+}) => {
+  const { createNode } = actions
+
+    let shipsleft = true;
+    let currentpage = 1;
+    let ships = [];
+
+    while (shipsleft) {
+        let shippages = await getShipsPage(currentpage); 
+        ships.push(...shippages.results);
+        if (!shippages?.next) {
+            shipsleft = false;
+        } else {
+            currentpage++;
+        }
+    }
+    
+  // loop through data and create Gatsby nodes
+  ships.forEach(ship =>
+    createNode({
+      ...ship,
+      id: createNodeId(`${SHIP_NODE_TYPE}-${ship.name}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: SHIP_NODE_TYPE,
+        content: JSON.stringify(ship),
+        contentDigest: createContentDigest(ship),
+      },
+    })
+  )
+
+  return
+}
+
+async function getShipsPage(page = 1) {
+    const ships = await axios.get(`https://swapi.dev/api/starships/?page=${page}`);
+    return ships.data;
+}
